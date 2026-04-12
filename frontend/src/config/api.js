d51b6644471d2d367bd38a3eb62fccb306223a9a
alakhagarwal <agarwalalakh2004@gmail.com>
2026-04-12 12:26:51 +0530

    Dynamic url in backend

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:8080',
-});
+export const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 
-export const baseURL = 'http://localhost:8080';
+const api = axios.create({
+  baseURL: baseURL,
+});
 
 export default api;
